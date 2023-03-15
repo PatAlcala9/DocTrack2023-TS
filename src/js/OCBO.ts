32c029dfdd04914252ffe0cd022e362a35384088
PatCode9 <45809622+PatCode9@users.noreply.github.com>
2023-03-15 11:19:30 +0800

    update encryption

--- src/js/OCBO.ts
+++ src/js/OCBO.ts
@@ -1,46 +1,144 @@
-const GenerateMD5 = require('md5')
+import * as GenerateMD5 from 'md5'
 
-export function encrypt(Password: string) {
-  const salt = 'ocboSalt@2023'
-  const md51 = GenerateMD5(salt + Password + salt)
+export function encrypt(Password: string, salt: string, iteration: number = 1, bits: 128 | 256 | 512 | 1024 | 2048) {
+  let expandedSalt = ''
+  for (let i = 0; i < iteration; i++) {
+    expandedSalt += salt;
+  }
+
+  const md51 = GenerateMD5(expandedSalt + Password + expandedSalt)
   let vowelCount = 0
   let consCount = 0
 
-  const p1 = md51.substr(0, 8)
-  const p2 = md51.substr(8, 8)
-  const p3 = md51.substr(16, 8)
-  const p4 = md51.substr(24, 8)
+  const p1 = md51.toString().substr(0, 8)
+  const p2 = md51.toString().substr(8, 8)
+  const p3 = md51.toString().substr(16, 8)
+  const p4 = md51.toString().substr(24, 8)
 
-  for (let i = 0; i < Password.length; i++) {
-    if (Password.charAt(i) === 'a' || Password.charAt(i) === 'A' || Password.charAt(i) === 'e' || Password.charAt(i) === 'E' || Password.charAt(i) === 'i' || Password.charAt(i) === 'I' || Password.charAt(i) === 'o' || Password.charAt(i) === 'O' || Password.charAt(i) === 'u' || Password.charAt(i) === 'U') {
-      vowelCount++
-    } else {
-      consCount++
+
+  for (let x = 0; x < iteration; x++) {
+    for (let i = 0; i < Password.length; i++) {
+      if (Password.charAt(i) === 'a' || Password.charAt(i) === 'A' || Password.charAt(i) === 'e' || Password.charAt(i) === 'E' || Password.charAt(i) === 'i' || Password.charAt(i) === 'I' || Password.charAt(i) === 'o' || Password.charAt(i) === 'O' || Password.charAt(i) === 'u' || Password.charAt(i) === 'U') {
+        vowelCount++
+      } else {
+        consCount++
+      }
     }
   }
+
+  const outputLength = bits / 128
+  let result = ''
 
   const xtra = consCount.toString() + vowelCount.toString()
   const newConsCount = consCount * parseInt(xtra)
   const newVowelCount = vowelCount * parseInt(xtra)
-  const newValue = Password + newConsCount + newVowelCount
+  const newValue = md51 + newConsCount + newVowelCount
   const md52 = GenerateMD5(newValue)
 
-  const p21 = md52.substr(0, 8)
-  const p22 = md52.substr(8, 8)
-  const p23 = md52.substr(16, 8)
-  const p24 = md52.substr(24, 8)
+  const p21 = md52.toString().substr(0, 8)
+  const p22 = md52.toString().substr(8, 8)
+  const p23 = md52.toString().substr(16, 8)
+  const p24 = md52.toString().substr(24, 8)
 
-  return p2 + p22 + p1 + p21 + p4 + p24 + p3 + p23
-}
+  result = p2 + p22 + p1 + p21 + p4 + p24 + p3 + p23
 
-export function comparePassword(dbPassword: string, strPassword: string) {
-  let result
-  const estrPassword = encrypt(strPassword)
+  if (outputLength > 1) {
+    const xtra = (consCount * outputLength).toString() + (vowelCount * outputLength).toString()
+    const newConsCount = (consCount * outputLength) * parseInt(xtra)
+    const newVowelCount = (vowelCount * outputLength) * parseInt(xtra)
+    const newValue = result + newConsCount + newVowelCount
+    const md53 = GenerateMD5(newValue)
+    const md54 = GenerateMD5(newConsCount.toString() + newVowelCount.toString())
 
-  if (dbPassword === estrPassword) {
-    result = true
-  } else {
-    result = false
+    const p31 = md53.toString().substr(0, 8)
+    const p32 = md53.toString().substr(8, 8)
+    const p33 = md53.toString().substr(16, 8)
+    const p34 = md53.toString().substr(24, 8)
+
+    const p31x = md54.toString().substr(0, 8)
+    const p32x = md54.toString().substr(8, 8)
+    const p33x = md54.toString().substr(16, 8)
+    const p34x = md54.toString().substr(24, 8)
+
+    result = result + p32x + p32 + p31x + p31 + p34x + p34 + p33x + p33
   }
+  
+  if (outputLength > 2) {
+    const xtra = (consCount * outputLength).toString() + (vowelCount * outputLength).toString()
+    const newConsCount = (consCount * outputLength) * parseInt(xtra)
+    const newVowelCount = (vowelCount * outputLength) * parseInt(xtra)
+    const newValue = result + newConsCount + newVowelCount
+    const md55 = GenerateMD5(newValue)
+    const md56 = GenerateMD5(newConsCount.toString() + newVowelCount.toString())
+
+    const p41 = md55.toString().substr(0, 8)
+    const p42 = md55.toString().substr(8, 8)
+    const p43 = md55.toString().substr(16, 8)
+    const p44 = md55.toString().substr(24, 8)
+
+    const p41x = md56.toString().substr(0, 8)
+    const p42x = md56.toString().substr(8, 8)
+    const p43x = md56.toString().substr(16, 8)
+    const p44x = md56.toString().substr(24, 8)
+
+    result = result + p42x + p42 + p41x + p41 + p44x + p44 + p43x + p43
+  }
+
+  if (outputLength > 3) {
+    const xtra = (consCount * outputLength).toString() + (vowelCount * outputLength).toString()
+    const newConsCount = (consCount * outputLength) * parseInt(xtra)
+    const newVowelCount = (vowelCount * outputLength) * parseInt(xtra)
+    const newValue = result + newConsCount + newVowelCount
+    const md57 = GenerateMD5(newValue)
+    const md58 = GenerateMD5(newConsCount.toString() + newVowelCount.toString())
+
+    const p51 = md57.toString().substr(0, 8)
+    const p52 = md57.toString().substr(8, 8)
+    const p53 = md57.toString().substr(16, 8)
+    const p54 = md57.toString().substr(24, 8)
+
+    const p51x = md58.toString().substr(0, 8)
+    const p52x = md58.toString().substr(8, 8)
+    const p53x = md58.toString().substr(16, 8)
+    const p54x = md58.toString().substr(24, 8)
+
+    result = result + p52x + p52 + p51x + p51 + p54x + p54 + p53x + p53
+  }
+
+  if (outputLength > 4) {
+    const xtra = (consCount * outputLength).toString() + (vowelCount * outputLength).toString()
+    const newConsCount = (consCount * outputLength) * parseInt(xtra)
+    const newVowelCount = (vowelCount * outputLength) * parseInt(xtra)
+    const newValue = result + newConsCount + newVowelCount
+    const md59 = GenerateMD5(newValue)
+    const md510 = GenerateMD5(newConsCount.toString() + newVowelCount.toString())
+
+    const p61 = md59.toString().substr(0, 8)
+    const p62 = md59.toString().substr(8, 8)
+    const p63 = md59.toString().substr(16, 8)
+    const p64 = md59.toString().substr(24, 8)
+
+    const p61x = md510.toString().substr(0, 8)
+    const p62x = md510.toString().substr(8, 8)
+    const p63x = md510.toString().substr(16, 8)
+    const p64x = md510.toString().substr(24, 8)
+
+    result = result + p62x + p62 + p61x + p61 + p64x + p64 + p63x + p63
+  }
+
   return result
 }
+
+// export function comparePassword(dbPassword: string, strPassword: string) {
+//   let result
+//   const estrPassword = encrypt(strPassword)
+
+//   if (dbPassword === estrPassword) {
+//     result = true
+//   } else {
+//     result = false
+//   }
+//   return result
+// }
+
+console.log(encrypt("hello", "ocbo123456", 4,  512))
