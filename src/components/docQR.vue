559ca26430a42de93145ce6016f87231b945a966
PatCode9 <45809622+PatCode9@users.noreply.github.com>
2023-05-17 16:59:39 +0800

    update

---
+++ src/components/docQR.vue
@@ -0,0 +1,32 @@
+<!-- <template>
+  <div>
+    <div ref="qrCodeRegion"></div>
+    <div>Decoded Text: {{ decodedText }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue';
+import Html5Qrcode from 'html5-qrcode';
+
+const qrCodeRegion = ref<HTMLElement | null>(null);
+const decodedText = ref('');
+
+onMounted(() => {
+  const qrCodeScanner = new Html5Qrcode('qr-code-full-region');
+
+  qrCodeScanner.start({ facingMode: 'environment' }, (decodedText) => {
+    // Update the decoded text
+    decodedText.value = decodedText;
+  });
+
+  // Set the qrCodeRegion ref to the QR code region element
+  qrCodeRegion.value = document.getElementById('qr-code-full-region');
+});
+
+onUnmounted(() => {
+  // Stop the QR code scanning when the component is unmounted
+  const qrCodeScanner = new Html5Qrcode('qr-code-full-region');
+  qrCodeScanner.stop();
+});
+</script> -->
--- src/pages/inquiry/QRScannerPage.vue
+++ src/pages/inquiry/QRScannerPage.vue
@@ -5,28 +5,27 @@ q-page(padding)
     span.title QR Scanner
     q-btn(flat size="md" label="Back" @click="gotoHome" icon="arrow_back").close-button
 
-  div.flex.flex-center
-    component(:is="VueQrReader" @code-scanned="handleDecode" @error-captured="handleError" videoWidth="300" videoHeight="300" :responsive="true")
+  div.full-width.column.wrap.justify-center.items-center.content-center
+    component.scanner(:is="QrStream" @decode="handleDecode" @error="handleError")
+    span {{ decodedText }}
 
 </template>
 
-<script setup lang="ts">
-// import { QrcodeStream } from 'vue-qrcode-reader'
-import VueQrReader from 'vue-qr-reader/dist/lib/vue-qr-reader.umd.js'
+<script setup>
+import { QrStream } from 'vue3-qr-reader'
+
 import { ref } from 'vue'
 import { api } from 'boot/axios'
 import { useRouter } from 'vue-router'
-import { useCurrentPage}  from 'stores/currentpage'
+import { useCurrentPage } from 'stores/currentpage'
 
-import docButton from 'components/docButton.vue'
+let decodedText = ref('')
 
 const router = useRouter()
 const _currentpage = useCurrentPage()
 
-
-
-const handleDecode = (text: string) => {
-  console.log(text)
+const handleDecode = (text) => {
+  decodedText.value = text
 }
 
 const handleError = () => {
@@ -38,3 +37,9 @@ const gotoHome = () => {
   router.push('/')
 }
 </script>
+
+<style lang="sass">
+.scanner
+  border: 1px solid #ffffff
+  border-radius: 5rem
+</style>
