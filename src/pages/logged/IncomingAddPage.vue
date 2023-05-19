5c536e278c69f1910063bc29ae8158afd91b16a2
Patrick Alvin Alcala <45809622+PatCode9@users.noreply.github.com>
2023-05-19 23:13:18 +0800

    update

--- src/pages/logged/IncomingAddPage.vue
+++ src/pages/logged/IncomingAddPage.vue
@@ -278,11 +278,11 @@ const saveNewIncoming = async () => {
   font-family: 'OpenSans'
   font-size: 1.6rem
   border-radius: 12px
-  ::v-deep .q-date__calendar-item
+  :deep .q-date__calendar-item
     padding-top: 0.6rem
     // padding-left: 0.5rem
     // padding-right: 0.5rem
-  ::v-deep .block
+  :deep .block
     font-size: 1.25rem
 
 
