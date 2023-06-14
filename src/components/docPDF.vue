ba5645c519626c2a337b0b00c360f1b65cb40e2f
Patrick Alvin Alcala <45809622+PatCode9@users.noreply.github.com>
2023-06-14 16:18:58 +0800

    update

--- src/components/docPDF.vue
+++ src/components/docPDF.vue
@@ -33,45 +33,46 @@ const sampleQR = ref(`**SCAN ME USING OCBO DOCTRACK** QrId::${encryptAES(randomD
 const qrSize = 200
 
 const createPDF = async () => {
-  const filePath = '../assets/wso.pdf'
-  const existingPdfBytes = fs.readFileSync(filePath)
-  const pdfDoc = await PDFDocument.load(existingPdfBytes)
+  // const filePath = '~/assets/wso.pdf'
+  // const existingPdfBytes = await fetch(filePath).then(res => res.arrayBuffer())
+  // const bytes = new Uint8Array(existingPdfBytes)
+  // const pdfDoc = await PDFDocument.load(bytes)
 
 
 
 
 
 
-  // const item = document.getElementById('qr')
-  // const image = item?.getAttribute('src')
-  // const link = document.createElement('a')
-  // link.href = image ?? ''
+  const item = document.getElementById('qr')
+  const image = item?.getAttribute('src')
+  const link = document.createElement('a')
+  link.href = image ?? ''
 
-  // const pdfDoc = await PDFDocument.create()
-  // const pngImage = await pdfDoc.embedPng(link.href)
-  // const pngDims = pngImage.scale(0.5)
-  // const page = pdfDoc.addPage(PageSizes.Letter)
+  const pdfDoc = await PDFDocument.create()
+  const pngImage = await pdfDoc.embedPng(link.href)
+  const pngDims = pngImage.scale(0.5)
+  const page = pdfDoc.addPage(PageSizes.Letter)
 
-  // const font = await pdfDoc.embedFont(StandardFonts.Helvetica)
-  // const textSize = 14
-  // // page.drawText('Falcon Heavy', { x: 120, y: 560, size: textSize, font })
-  // // page.drawText('Saturn IV', { x: 120, y: 500, size: textSize, font })
-  // // page.drawText('Delta IV Heavy', { x: 340, y: 560, size: textSize, font })
-  // // page.drawText('Space Launch System', { x: 340, y: 500, size: textSize, font })
+  const font = await pdfDoc.embedFont(StandardFonts.Helvetica)
+  const textSize = 14
+  // page.drawText('Falcon Heavy', { x: 120, y: 560, size: textSize, font })
+  // page.drawText('Saturn IV', { x: 120, y: 500, size: textSize, font })
+  // page.drawText('Delta IV Heavy', { x: 340, y: 560, size: textSize, font })
+  // page.drawText('Space Launch System', { x: 340, y: 500, size: textSize, font })
 
-  // page.drawImage(pngImage, {
-  //   x: page.getWidth() / 4 - pngDims.width / 2 + 75,
-  //   y: page.getHeight() / 1 - pngDims.height,
-  //   width: pngDims.width,
-  //   height: pngDims.height,
-  // })
+  page.drawImage(pngImage, {
+    x: page.getWidth() / 4 - pngDims.width / 2 + 75,
+    y: page.getHeight() / 1 - pngDims.height,
+    width: pngDims.width,
+    height: pngDims.height,
+  })
 
-  // pdfDoc.setTitle(props.title)
+  pdfDoc.setTitle(props.title)
 
-  // const pdfBytes = await pdfDoc.save()
-  // const pdfBlob = new Blob([pdfBytes], { type: 'application/pdf' })
-  // const link2 = document.createElement('a')
-  // link2.href = URL.createObjectURL(pdfBlob)
-  // window.open(link2.href)
+  const pdfBytes = await pdfDoc.save()
+  const pdfBlob = new Blob([pdfBytes], { type: 'application/pdf' })
+  const link2 = document.createElement('a')
+  link2.href = URL.createObjectURL(pdfBlob)
+  window.open(link2.href)
 }
 </script>
