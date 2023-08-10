004889993968c91a5d256363b128ee2c09227766
Patrick Alvin Alcala <45809622+PatCode9@users.noreply.github.com>
2023-08-10 15:13:00 +0800

    08/10/2023 - Version change

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -121,7 +121,7 @@ let userid = 0
 let employeeName = null
 let loginSuccess = false
 
-const version = ref('v 0.3')
+const version = ref('v 0.4')
 
 const showInquiry = async () => {
   await exitInquiry()
