740461228ffc14ec30c51c57ff2bba02d63e39bd
Patrick Alvin Alcala <45809622+PatCode9@users.noreply.github.com>
2023-09-04 11:40:00 +0800

    09/04/2023 - New Page

--- src/pages/NotReadyPage.vue
+++ src/pages/NotReadyPage.vue
@@ -1,11 +1,24 @@
 <template lang="pug">
 
-q-page(padding).flex.flex-center
-  h2 This page is not ready
-  component(:is="DocButton" label="aa")
+q-page(padding).column.fit.justify-center.content-center.items-center
+  h2 This page is still in development
+  component(:is="DocButton" text="Go Back" @click="gotoPage('dashboard')")
 
 </template>
 
 <script setup lang="ts">
 import DocButton from 'components/docButton.vue'
+import { useCurrentPage } from 'stores/currentpage'
+import { usePageWithTable } from 'stores/pagewithtable'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const _currentpage = useCurrentPage()
+const _pagewithtable = usePageWithTable()
+
+const gotoPage = (page: string, table = false) => {
+  _pagewithtable.pagewithtable = table
+  _currentpage.currentpage = page
+  router.push(page)
+}
 </script>
