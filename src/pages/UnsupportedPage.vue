1b8d23b0e194b7e9a81a2d96cd65282b6cd3a42f
Patrick Alvin Alcala <45809622+PatCode9@users.noreply.github.com>
2023-09-07 23:15:00 +0800

    09/07/2023 - New component

--- src/pages/UnsupportedPage.vue
+++ src/pages/UnsupportedPage.vue
@@ -1,9 +1,14 @@
 <template lang="pug">
 
-q-page(padding).column.fit.justify-center.content-center.items-center.text-center
-  h2 Browser
-  h4 {{ state.browserName }}
-  component(:is="DocButton" text="Go Back" @click="gotoPage('dashboard')")
+q-page(padding).column.fit.justify-center.content-center.items-center.text-center.ns-page
+  span.main Browser Not Supported
+  span.sub--main Doctrack was built using the latest web technologies which your current browser does not fully support.
+  span.sub--main Please consider upgrading to a newer browser version for the best experience.
+
+  span.info Browser Info: {{ state.browserName }} version {{ state.browserVersion }}
+
+  section.button
+    component(:is="DocButton" text="Go Back" @click="gotoPage('dashboard')")
 
 </template>
 
@@ -25,17 +30,46 @@ const gotoPage = (page: string, table = false) => {
   router.push(page)
 }
 
+const capitalize = (str: string) => {
+  return str[0].toUpperCase() + str.slice(1)
+}
 
-const state = reactive({
+interface State {
+  browserName: string
+  browserVersion: number
+  devicePlatform: string
+}
+
+const state = reactive<State>({
   browserName: '',
-  // browserVersion: '' | 0,
-  devicePlatform: ''
+  browserVersion: 0,
+  devicePlatform: '',
 })
 
 const detectBrowser = () => {
-  state.browserName = Platform.is.name
-  // state.browserVersion = Platform.is.version ?? 0
+  state.browserName = capitalize(Platform.is.name)
+  state.browserVersion = Number(Platform.is.version ?? 0)
   state.devicePlatform = Platform.is.platform
 }
+
+detectBrowser()
 </script>
 
+<style lang="sass" scoped>
+.main
+  font-size: 4rem
+
+.sub--main
+  font-size: 1.6rem
+
+.ns-page
+  background-color: black
+  font-family: 'Inter'
+
+.info
+  margin-top: 3rem
+  font-size: 1.2rem
+
+.button
+  padding-top: 2rem
+</style>
