b81af9ea960c6b661b79b7501a94843f64ec2521
Patrick Alvin Alcala <45809622+PatCode9@users.noreply.github.com>
2023-09-29 16:28:00 +0800

    09/29/2023 - New version 0.12

--- src/components/docVersions.vue
+++ src/components/docVersions.vue
@@ -8,8 +8,9 @@ div.full-width.column.wrap.justify-start.items-start.content-start
 </template>
 
 <script setup lang="ts">
-const versions = ['0.11', '0.10', '0.9', '0.8', '0.7', '0.6', '0.5', '0.4', '0.3', '0.2', '0.1', 'Undocumented']
+const versions = ['0.12','0.11', '0.10', '0.9', '0.8', '0.7', '0.6', '0.5', '0.4', '0.3', '0.2', '0.1', 'Undocumented']
 const infos = {
+  '0.12': ['09/29/2023 - Registration highlighted to red when empty', '09/29/2023 - Added color variables for fast color switching', '09/29/2023 - Complaint Add highlighted to red when empty (partially)'],
   '0.11': ['09/27/2023 - Added Highligh of expired data', '09/27/2023 - Revised complaint list filter', '09/26/2023 - Fixed Expiration countdown', '09/26/2023 - Added Pop-up when missing details', '09/25/2023 - Added recording of Employee Id', '09/25/2023 - Fixed lowercase value on saved data', '09/25/2023 - Changed checking of connection to 1 minute', '09/25/2023 - Added Edit log'],
   '0.10': ['09/22/2023 - Fixed Respondent saving', '09/18/2023 - Changed name to DDMS', '09/16/2023 - Added API for work stoppage', '09/16/2023 - Used the new logo for PDF'],
   '0.9': ['09/11/2023 - New Background', '09/11/2023 - Patched Security', '09/11/2023 - Removed Mobile Bug', '09/11/2023 - Changed mode to SPA', '09/11/2023 - Updated NPM', '09/11/2023 - Used new logo on README', '09/11/2023 - Fixed Browser detection and add loading when switching pages'],
